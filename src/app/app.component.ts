5f3d1e977bcc4d2351070784c5d2d0a55b0e785f
Napstyyy <mate2003-4@outlook.com>
2023-01-31 09:48:58 -0500

    Directivas learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.models';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de Empleados';
+  empleados:Empleado[]=[
+
+    new Empleado("Mateo","Giraldo","Developper",1000),
+    new Empleado("Anderson","Avarte","Developper",1000),
+    new Empleado("Cristobal","Colon","Developper",1000),
+    new Empleado("Juan","Camilo","Developper",1000),
+
+
+  ];
+
+  agregarEmpleado(){
+
+    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+
+  }
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
 }
